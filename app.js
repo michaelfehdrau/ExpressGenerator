bbb341afe970daec4a596347ecfdb5060a1f2aab
Michael Fehdrau <mfehdrau@hotmail.com>
2023-08-04 00:35:14 -0400

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,26 +47,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //authentication function begins here
 function auth(req, res, next) {
-  console.log(req.session);
+    console.log(req.user);
 
-  if (!req.session.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    if (req.session.user === 'authenticated') {
-      return next();
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
+        return next();
     }
-  }
 }
 
 app.use(auth);
